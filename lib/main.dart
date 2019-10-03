80a129e9fd61f98ba4486e78abb1722aba5ec5d2
Varun Resh Maddali <mvresh@gmail.com>
2019-10-03 18:57:50 +0530

    using the id instead of link.

--- lib/main.dart
+++ lib/main.dart
@@ -38,31 +38,31 @@ class _MoviesPageState extends State<MoviesPage> {
 
   int movieItemNumber = 1;
 
-  // to track and initiate the next link when the button gets clicked. It's starting with 1 because first link is already provided. Another way to do it was to use the ID.
+  // to track and initiate the next link when the button gets clicked. It's starting with 1 because first link is already provided. Another way to do it was to use the ID. I changed the code to use this.
 
-  var movieLink = 'https://raw.githubusercontent.com/android10/Sample-Data/master/Android-CleanArchitecture-Kotlin/posters/038001.jpg';
+  var movieLinkId = '038001';
 
   // first movie link is needed because from setState gets activated only when button gets clicked. For first time , it needs to be provided in raw.
 
   // below function gets triggered when button gets clicked
 
-  changeLink() {
+  changeMovieLinkId() {
 
     setState(() {
 
       // printing each link to the console
 
-      print(decodedMoviesList[movieItemNumber]["poster"]);
+        print(decodedMoviesList[movieItemNumber]["id"]);
 
       // updating movieLink when the button is pressed
 
-      movieLink = decodedMoviesList[movieItemNumber]["poster"];
-
-      //updating item number in the list so that next link can be accessed
-
-      movieItemNumber = movieItemNumber + 1;
+        movieLinkId = decodedMoviesList[movieItemNumber]["id"];
 
     });
+
+    //updating item number in the list so that next link can be accessed
+
+    movieItemNumber = movieItemNumber + 1;
 
   }
 
@@ -78,12 +78,12 @@ class _MoviesPageState extends State<MoviesPage> {
                 SizedBox(height: 50),
                 Center(
                     child: Container(
-                        width: 350, height: 350, child: Image.network('${movieLink}'))),
+                        width: 350, height: 350, child: Image.network('https://raw.githubusercontent.com/android10/Sample-Data/master/Android-CleanArchitecture-Kotlin/posters/$movieLinkId.jpg'))),
                 SizedBox(height: 50),
                 FlatButton(
                     color: Colors.red,
                     textColor: Colors.white,
-                    onPressed: changeLink,
+                    onPressed: changeMovieLinkId,
                     child: Text(
                         'Next Movie',
           ),
