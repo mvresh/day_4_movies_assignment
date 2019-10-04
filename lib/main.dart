efbe5f275c4fb2c43f73c96c9a6511ac5755ed82
Varun Resh Maddali <mvresh@gmail.com>
2019-10-04 14:34:50 +0530

    carousel exercise

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,20 @@
 import 'package:day_4_movies_assignment/movies.dart';
 import 'package:flutter/material.dart';
 import 'dart:convert';
+import 'package:carousel_slider/carousel_slider.dart';
 
+List imgList = new List();
 void main() {
+  String moviesList = MoviesList.moviesJsonList;
+  List decodedMoviesList = json.decode(moviesList);
+
+  int i = 0;
+  while(i != decodedMoviesList.length){
+    imgList.add(decodedMoviesList[i]["poster"]);
+    i++;
+  }
+  print(imgList);
+
   runApp(MaterialApp(
     home: Scaffold(
       appBar: AppBar(
@@ -12,83 +24,48 @@ void main() {
         ),
         backgroundColor: Colors.yellow,
       ),
-      body: MoviesPage(), // Use stateful widget you created here
+      body: CarouselMovies(), // Use stateful widget you created here
     ),
   ));
 }
 
 // You are given json string of movies list (see file movies.dart)
-String moviesList = MoviesList.moviesJsonList;
+
 // Hint:
 // Convert the string to List of maps using jsonDecode and then use it
 
 // Create a stateful widget called MoviesPage here
+List<T> map<T>(List list, Function handler) {
+  List<T> result = [];
+  for (var i = 0; i < list.length; i++) {
+    result.add(handler(i, list[i]));
+  }
 
-class MoviesPage extends StatefulWidget {
-  @override
-  _MoviesPageState createState() => _MoviesPageState();
+  return result;
 }
 
-class _MoviesPageState extends State<MoviesPage> {
+final List child = map<Widget>(
+  imgList,
+      (index, i) {
+    return Container(
+      child: Image.network(i),
+    );
+  },
+).toList();
 
-
-  List decodedMoviesList = json.decode(moviesList);
-
-  // A lot of time got wasted here as I tried to convert the list to a map so that each element could be accessed iteratively. But later it occurred that list[][] can access the same
-
-  int movieItemNumber = 1;
-
-  // to track and initiate the next link when the button gets clicked. It's starting with 1 because first link is already provided. Another way to do it was to use the ID. I changed the code to use this.
-
-  var movieLinkId = '038001';
-
-  // first movie link is needed because from setState gets activated only when button gets clicked. For first time , it needs to be provided in raw.
-
-  // below function gets triggered when button gets clicked
-
-  changeMovieLinkId() {
-
-    setState(() {
-
-      // printing each link to the console
-
-        print(decodedMoviesList[movieItemNumber]["id"]);
-
-      // updating movieLink when the button is pressed
-
-        movieLinkId = decodedMoviesList[movieItemNumber]["id"];
-
-    });
-
-    //updating item number in the list so that next link can be accessed
-
-    movieItemNumber = movieItemNumber + 1;
-
-  }
+class CarouselMovies extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
-    return Container(
-
-        child: Column(
-
-                children: <Widget>[
-
-                SizedBox(height: 50),
-                Center(
-                    child: Container(
-                        width: 350, height: 350, child: Image.network('https://raw.githubusercontent.com/android10/Sample-Data/master/Android-CleanArchitecture-Kotlin/posters/$movieLinkId.jpg'))),
-                SizedBox(height: 50),
-                FlatButton(
-                    color: Colors.red,
-                    textColor: Colors.white,
-                    onPressed: changeMovieLinkId,
-                    child: Text(
-                        'Next Movie',
-          ),
-        ),
-      ],
-    ));
+    return CarouselSlider(
+      height: 500,
+      items: child,
+      autoPlay: false,
+      enlargeCenterPage: true,
+      viewportFraction: 0.9,
+      aspectRatio: 16/9,
+    );
   }
 }
+
+
